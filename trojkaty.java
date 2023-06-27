ecf6f679e1a30fa1aaa001556934905113f927c0
Mateusz Robert Badura <mabadura@artemis.wszib.edu.pl>
2023-06-27 09:43:03 +0200

    Weryfikacja wejścia: warunek istnienia trójkąta.

--- trojkaty.java
+++ trojkaty.java
@@ -21,12 +21,19 @@ class Trojkaty {
         System.out.println("Program do rozpoznawania rodzaju trójkąra");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
-    /** Glowna funkcja */
-    public static void main(String... args) {
-        if (args.length != 3) {
-            pomoc();
-            System.exit(1);
-        }
+   public static boolean czyIstniejeTrojkat(float a, float b, float c){
+                  if (a + b <= c) return false;
+                  if (a + c <= b) return false;
+                  if (b + c <= a) return false;
+                  return true;
+               }
+               /** Glowna funkcja */
+               public static void main(String... args) {
+                   ...
+                   if (!czyIstniejeTrojkat(a, b, c)) {
+                    System.out.println("Z podanych długości boków nie da się zbudować trójkąta");
+                    System.exit(3);
+                   }
         float a = Float.valueOf(args[0]);
         float b = Float.valueOf(args[1]);
         float c = Float.valueOf(args[2]);
